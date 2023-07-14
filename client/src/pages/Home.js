cd39585c06f217af1d1ac7fd4e2662cfb894e2bb
HemantDutta <hemantnorddutta2@gmail.com>
2023-07-14 12:51:30 +0530

    Create hero left

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,32 @@
 import {Navbar} from "../components/Navbar";
+import {Link} from "react-router-dom";
 
-export const Home = () =>{
-    return(
+export const Home = () => {
+    return (
         <>
             <div className="header-container">
                 <Navbar/>
             </div>
             <div className="home">
-
+                <section className="hero" id="hero">
+                    <div className="hero-container">
+                        <div className="hero-left">
+                            <div className="hero-left-header">
+                                <span>Unleash the Power of Web Scraping for Personalized Tech News!</span>
+                            </div>
+                            <div className="hero-left-search">
+                                <div className="search-container">
+                                    <input type="text" name="search" id="search" placeholder="Explore Latest Tech Updates..."/>
+                                    <i className="fa-solid fa-magnifying-glass"/>
+                                </div>
+                            </div>
+                            <div className="hero-left-cta">
+                                <Link to="/web-scraping">Learn more about web scraping&nbsp;<i className="fa-solid fa-arrow-right"/></Link>
+                            </div>
+                        </div>
+                        <div className="hero-right"></div>
+                    </div>
+                </section>
             </div>
         </>
     )
